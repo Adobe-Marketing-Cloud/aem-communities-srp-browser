30b7cc50dde0ec3844f44928fd4554efbce9dfd0
Wyatt Cooper <wcooper@adobe.com>
2016-07-01 15:29:00 -0700

    front-end recursive tree navigation of /content root

--- content/jcr_root/apps/srp-tools/components/hbs/browser/clientlibs/browser.js
+++ content/jcr_root/apps/srp-tools/components/hbs/browser/clientlibs/browser.js
@@ -3,6 +3,7 @@
     var Browser = SCF.Model.extend({
         modelName: "BrowserModel",
         defaults: {
+            navData: [],
             path: "/",
             json: {},
             jsonString: ""
@@ -12,10 +13,50 @@
             var that = this;
             $.get(url, function (response) {
                 that.set("json", response);
-//                that.trigger('ugc:fetched', {
-//                    'model': that
-//                });                
+                that.trigger('ugc:fetched', {
+                    'model': that
+                });                
             });
+        },
+        loadFirst: function () {
+            var url = SCF.config.urlRoot + this.get('id') + ".srp.json?path=" + "/content/";
+            var that = this;
+            $.ajax({
+                type: 'GET',
+                url: url,
+                async: false, 
+                success: function (response) {
+                    that.set("json", response);
+                }
+                });
+        },
+        loadTreeData: function(path) {
+            var that = this;
+            var json = {};
+            var url = SCF.config.urlRoot + this.get('id') + ".srp.json?path=" + path;
+            $.ajax({
+                type: 'GET',
+                url: url,
+                async: false, 
+                success: function (response) {
+                    json = response;
+                }
+                });
+            var tree = [];
+            if (json.path){
+                if (json.children){
+                    var i = 0;
+                    for (i = 0; i < json.children.length; i++){
+                        var newNode = {text: "", nodes: []};
+                        var path = json.children[i].path;
+                        var final = path.substr(path.lastIndexOf('/') + 1); 
+                        newNode.text = final;
+                        newNode.nodes = that.loadTreeData(path);
+                        tree[i] = newNode;      
+                    }
+                }
+            }
+            return tree;
         }
     });
     var BrowserView = SCF.View.extend({
@@ -23,14 +64,15 @@
         tagName: "div",
         className: "scf-browser",
         init: function () {
-            this.listenTo(this.model, "ugc:fetched", this.render);
+            //this.listenTo(this.model, "ugc:fetched", this.render);
+            this.buildNav();
         },  
         fetch: function (e) {
             e.preventDefault();
             this.model.set("path", this.getField("path"));
             this.model.loadUGC();
             const formatter = new JSONFormatter(this.model.get("json"));
-            var tree = document.getElementById("tree");
+            var tree = document.getElementById("json");
                 if (!tree.hasChildNodes()){
                     tree.appendChild(formatter.render());
                 }
@@ -39,20 +81,47 @@
                 }
             return false;
         },
-        displayBase: function(e) {
-            this.model.set("path", "/content");
-            const formatter = new JSONFormatter(this.model.get("json"));
-            var tree = document.getElementById("tree");
-                if (!tree.hasChildNodes()){
-                    tree.appendChild(formatter.render());
+        buildNav: function () {
+            //e.preventDefault();
+            var that = this;
+            this.model.set("path", this.getField("path"));
+            this.model.loadFirst();
+            var data = [];
+            var bigNode = this.model.loadTreeData("/content");
+            data = bigNode;
+            this.model.set("navData", data);
+            $('#tree').treeview({
+                data: this.model.get("navData"),
+                levels: 0,
+                onNodeSelected: function(event, node) {
+                    var path = "/content/" + that.breadcrumbs(node);
+                    $('#bcrumbs').attr("value", path);
+                    console.log(path);
                 }
-                else{
-                    tree.replaceChild(formatter.render(), document.getElementById("bigTree"));
-                }
+//                onNodeExpanded: function(event, node) {
+//                    if (!node.nodes.length) {
+//                        var path = "/content/" + that.breadcrumbs(node);
+//                        var newTree = that.model.loadTreeData(path);
+//                        node.nodes = newTree;
+//                        this.model.set("navData")
+//                    }
+//                }
+            });
+            return false;
+        },
+        breadcrumbs: function(node) {
+            var that = this;
+            //console.log(parent.text);
+            if (!(node.length)) {
+                var parent = $('#tree').treeview('getParent', node);
+                return node.text + "/" + that.breadcrumbs(parent);
+            }
+            else{
+                return "";
+            }
         }
     });
     SCF.Browser = Browser;
     SCF.BrowserView = BrowserView;
     SCF.registerComponent('srp-tools/components/hbs/browser', SCF.Browser, SCF.BrowserView);
-
 })(SCF);
